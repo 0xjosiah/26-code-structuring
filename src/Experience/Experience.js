4e3105428281524eb25f939bba7f7781d945172b
Josiah Webb <jj.webb520@gmail.com>
2022-11-07 13:52:11 -0700

    adds canvas as Experience constructor param

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,5 +1,10 @@
 export default class Experience {
-    constructor() {
+    constructor(canvas) {
+
+        // Global access
         window.experience = this // this can be dicey to use but useful, take care when implementing
+
+        // Options
+        this.canvas = canvas
     }
 }
