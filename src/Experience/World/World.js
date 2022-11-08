4443efd52e8592a505cee0665f91ff499bc5e5a6
Josiah Webb <jj.webb520@gmail.com>
2022-11-07 17:15:28 -0700

    creates environment class

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,15 +1,20 @@
 import * as THREE from 'three'
 import Experience from "../Experience";
+import Environment from './Environment';
 
 export default class World {
     constructor() {
         this.experience = new Experience()
         this.scene = this.experience.scene
 
+        // Test mesh
         const testMesh = new THREE.Mesh(
             new THREE.SphereGeometry(),
-            new THREE.MeshBasicMaterial(0xffffff)
+            new THREE.MeshStandardMaterial(0xffffff)
         )
         this.scene.add(testMesh)
+
+        // Setup
+        this.environment = new Environment()
     }
 }
