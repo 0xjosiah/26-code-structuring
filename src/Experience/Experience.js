0f1bfb4f01cd0fd0b891585d4e4c1f3a2368758a
Josiah Webb <jj.webb520@gmail.com>
2022-11-07 17:23:12 -0700

    creates Resources class

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -4,6 +4,7 @@ import Time from "./Utils/Time"
 import Camera from './Camera'
 import Renderer from './Renderer'
 import World from './World/World'
+import Resources from './Utils/Resources'
 
 let instance = null
 
@@ -23,6 +24,7 @@ export default class Experience {
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
+        this.resources = new Resources()
         this.camera = new Camera()
         this.renderer = new Renderer()
         this.world = new World()
