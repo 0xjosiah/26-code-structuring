dfc9d4d26222014fc844d8aea4f40e0859e55cdc
Josiah Webb <jj.webb520@gmail.com>
2022-11-09 14:29:24 -0700

    creates methods in floor class to add floor to scene

--- src/Experience/World/Floor.js
+++ src/Experience/World/Floor.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three'
 import Experience from "../Experience";
 
 export default class Floor {
@@ -6,6 +7,41 @@ export default class Floor {
         this.scene = this.experience.scene
         this.resources = this.experience.resources
 
+        this.setGeometry()
+        this.setTexture()
+        this.setMaterial()
+        this.setMesh()
+    }
+
+    setGeometry() {
+        this.geometry = new THREE.CircleGeometry(5, 64)
+    }
+
+    setTexture() {
+        this.textures = {}
+        this.textures.color = this.resources.items.dirtColorTexture
+        this.textures.color.encoding = THREE.sRGBEncoding
+        this.textures.color.repeat.set(1.5, 1.5)
+        this.textures.color.wrapS = THREE.RepeatWrapping
+        this.textures.color.wrapT = THREE.RepeatWrapping
         
+        this.textures.normal = this.resources.items.dirtNormalTexture
+        this.textures.normal.repeat.set(1.5, 1.5)
+        this.textures.normal.wrapS = THREE.RepeatWrapping
+        this.textures.normal.wrapT = THREE.RepeatWrapping
+    }
+
+    setMaterial() {
+        this.marterial = new THREE.MeshStandardMaterial({
+            map: this.textures.color,
+            normalMap: this.textures.normal
+        })
+    }
+
+    setMesh() {
+        this.mesh = new THREE.Mesh(this.geometry, this.marterial)
+        this.mesh.rotation.x = -Math.PI * .5
+        this.mesh.receiveShadow = true
+        this.scene.add(this.mesh)
     }
 }
