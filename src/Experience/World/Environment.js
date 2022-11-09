120cc6236d6edb846ac5416449367efa80e2d524
Josiah Webb <jj.webb520@gmail.com>
2022-11-09 14:12:27 -0700

    adds setEnvMap method to Environment, adds floor textures to sources.js

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -5,8 +5,10 @@ export default class Environment {
     constructor() {
         this.experience = new Experience()
         this.scene = this.experience.scene
+        this.resources = this.experience.resources
 
         this.setSunLight()
+        this.setEnvMap()
     }
 
     setSunLight() {
@@ -17,5 +19,26 @@ export default class Environment {
         this.sunLight.shadow.normalBias = 0.05
         this.sunLight.position.set(3.5, 2, - 1.25)
         this.scene.add(this.sunLight)
+    }
+
+    setEnvMap() {
+        this.envMap = {}
+        this.envMap.intensity = .4
+        this.envMap.texture = this.resources.items.envMapTexture
+        this.envMap.texture.encoding = THREE.sRGBEncoding
+
+        this.scene.environment = this.envMap.texture
+
+        this.setEnvMap.updateMaterial = () => {
+            this.scene.traverse((child) => {
+                if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+                    child.material.envMap = this.envMap.texture
+                    child.material.envMapIntensity = this.envMap.intensity
+                    child.material.needsUpdate = true
+                }
+            })
+        }
+
+        this.setEnvMap.updateMaterial()
     }
 }
